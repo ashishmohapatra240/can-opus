f685976f32137bc83cd28675da5de7c1612b5b67
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-11-03 11:45:52 +0530

    refactor: update main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,20 @@ import IndustriesSection from './components/IndustriesSection';
 import TeamSection from './components/TeamSection';
 import CallToActionSection from './components/CallToActionSection';
 import PartnersSection from './components/PartnersSection';
+import WhatWeDoSection from './components/WhatWeDoSection';
+import TestimonialsSection from './components/TestimonialsSection';
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <PartnersSection />
+      <WhatWeDoSection />
       <AboutSection />
       <ProductsSection />
       <ServicesSection />
       <TeamSection/>
+      <TestimonialsSection />
       <IndustriesSection />
       <CallToActionSection />
     </main>
